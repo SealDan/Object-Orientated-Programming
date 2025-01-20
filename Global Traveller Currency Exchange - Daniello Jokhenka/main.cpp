4fdd9672856d154e8fbcc35d75945aeabd2c4894
Daniello Jokhenka <daniello.jokhenka@gmail.com>
2025-01-20 09:45:49 +0000

    Update main.cpp

--- Global Traveller Currency Exchange - Daniello Jokhenka/main.cpp
+++ Global Traveller Currency Exchange - Daniello Jokhenka/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    const double USD_TO_GBP = 0.82, EURO_TO_GBP = 0.84, CNY_TO_GBP = 0.11, SGD_TO_GBP = 0.6, MYR_TO_GBP = 0.18;
-    double GBP = 0, USD = 0, EURO = 0, CNY = 0, SGD = 0, MYR = 0;
+    const double USD_TO_GBP = 0.82, EURO_TO_GBP = 0.84, CNY_TO_GBP = 0.11, SGD_TO_GBP = 0.6, MYR_TO_GBP = 0.18; //exchange variable
+    double GBP = 0, USD = 0, EURO = 0, CNY = 0, SGD = 0, MYR = 0; //initialize amount for each currency
 
     cout << "Please enter the amount of US Dollar: ";
     cin >> USD;
@@ -15,9 +15,11 @@ int main() {
     cin >> SGD;
     cout << "Please enter the amount of Malaysia Ringgit: ";
     cin >> MYR;
-
+    
+    //Exchange each currency to GBP and Sum
     GBP = USD * USD_TO_GBP + EURO * EURO_TO_GBP + CNY * CNY_TO_GBP + SGD * SGD_TO_GBP + MYR * MYR_TO_GBP;
 
+    //print result
     cout << "Total in British Pounds is " << GBP << endl;
 
     return 0;
